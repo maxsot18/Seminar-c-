1132dad52a05592277eab06afd6de50478da1234
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-26 06:36:48 -0800

    Task_05

---
+++ Task_05/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter a number N ");
+string InputValue = Console.ReadLine();
+int N = Convert.ToInt32(InputValue);
+int unN = -N;
+
+while(unN <= N)
+{
+    Console.Write(unN + " ");
+    unN = unN + 1;
+}
