f7fa9913f67d1ccf0606159ccf58891e7d10382f
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-26 04:35:11 -0800

    Task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("inter a first number ");
+string inputFirstValue = Console.ReadLine();
+int firstvalue = Convert.ToInt32(inputFirstValue);
+
+Console.Write("inter a second number ");
+string inputSecondValue = Console.ReadLine();
+int secondvalue = Convert.ToInt32(inputSecondValue);
+
+bool squareValue = secondvalue * secondvalue == firstvalue;
+Console.WriteLine("is first number square of "+ secondvalue + " ? " + squareValue);
