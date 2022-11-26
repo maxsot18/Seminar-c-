e8c6a8df876847bcbad74f96e4267004e7ec9a67
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-26 03:51:07 -0800

    TASK_01

---
+++ TASK_01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("inter a number ");
+string inputValue = Console.ReadLine();
+int value = Convert.ToInt32(inputValue);
+
+int squareValue = value * value;
+Console.WriteLine("Square of "+ value + "=" + squareValue);
+
