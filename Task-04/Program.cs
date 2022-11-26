0abbc72985a797734633da1a42ea859a957afadb
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-26 06:16:20 -0800

    Task_04

---
+++ Task-04/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter a number ");
+string inputValue = Console.ReadLine();
+int value = Convert.ToInt32(inputValue);
+
+int CubeValue = value * 3;
+Console.WriteLine("Cube of "+ value + "=" + CubeValue);
