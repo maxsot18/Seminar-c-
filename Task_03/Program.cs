9da6a05e5a509845824b35233c2de494c902d800
Maxsot <sotnikov.maxim3007@gmail.com>
2022-11-26 06:00:17 -0800

    Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Number of week day ");
+string InputValue = Console.ReadLine();
+int Value = Convert.ToInt32(InputValue);
+
+if(Value == 1)
+{
+    Console.Write("Monday");
+}
+if(Value == 2)
+{
+    Console.Write("Tuesday");
+} 
+if(Value == 3)
+{
+    Console.Write("Wednesday");
+}
+if(Value == 4)
+{
+    Console.Write("Thursday");
+}
+if(Value == 5)
+{
+    Console.Write("Friday");
+}
+if(Value == 6)
+{
+    Console.Write("Saturday");
+}
+if(Value == 7)
+{
+    Console.Write("Sunday");
+}
