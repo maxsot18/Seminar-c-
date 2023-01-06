e7526959db5229ab4816e0bbd62e557a8840ee63
Maxsot <sotnikov.maxim3007@gmail.com>
2023-01-05 23:43:05 -0800

    Task03

---
+++ 9/Task03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3: Напишите программу, которая будет принимать на вход число и возвращать 
+// сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumOfNumbers (int number)
+{
+    int sum = 0;
+    while(number % 10 > 0) //&& number / 10 > 0)
+    {
+        int n = number % 10;
+        sum += n;
+        number = number / 10;
+
+    }
+    Console.WriteLine($"sum of numbers = {sum}");
+}
+int n = Prompt("Enter a number ");
+SumOfNumbers(n);
