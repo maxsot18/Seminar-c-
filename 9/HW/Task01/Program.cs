94cd7cddca52ae60750d85054de21faa22743ff9
Maxsot <sotnikov.maxim3007@gmail.com>
2023-01-06 02:39:48 -0800

    Task01 of HW

---
+++ 9/HW/Task01/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные 
+// натуральные числа в промежутке от M до N с помощью рекурсии.
+
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void EvenNumbers (int m, int n)
+{
+    if(m == n && n % 2 > 0)
+    {
+        return;
+    }
+    if(m % 2 == 0)
+    {
+        Console.Write($"{m}\t");
+    }
+    EvenNumbers(m+1, n);
+}
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int number1 = Prompt("Enter first number ");
+int number2 = Prompt("Enter second number ");
+EvenNumbers(number1 , number2);
